ca972acac8af8471fd6aa587e43426e473b2d8a8
welchie <chris@weewelchie.org>
2023-10-18 08:16:18 +0100

    updating files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import NavBar from './NavBar';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import NotFoundPage from './pages/NotFoundPage';
+import LoginPage from './pages/LoginPage';
+import CreateAccountPage from './pages/CreateAccountPage';
+
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>My Awesome blog</h1>
+     <NavBar/>
+     <div id="page-body">
+      <Routes>
+       <Route path="/" element={<HomePage />} />
+       <Route path ="/about" element={<AboutPage />} />
+       <Route path="/articles" element = {<ArticlesListPage />} />
+       <Route path="/articles/:articleId" element={<ArticlePage />} />
+       <Route path="/login" element={<LoginPage/>} />
+       <Route path="/create-account" element={<CreateAccountPage/>} />
+       <Route path="*" element={<NotFoundPage/>}/>
+       </Routes>
+      </div>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,69 @@
+import { useState, useEffect } from 'react'; 
+import {useParams} from 'react-router-dom';
+import articles from './article-content';
+import axios from 'axios';
+import NotFoundPage from './NotFoundPage';
+import CommentsList from '../components/CommentsList';
+import AddCommentForm from '../components/AddCommentForm';
+import useUser from '../hooks/useUser';
+
+
+const ArticlePage = () => {
+    const [articleInfo, setArticleInfo] = useState ({ upvotes: 0 , comments: []});
+    const { articleId } = useParams();
+    console.log("Artcle ID: " + articleId);
+
+    const {user,isLoading} = useUser();
+
+   useEffect(() => {
+            const loadArticleInfo = async() =>
+            {
+                console.log("Artcle ID2: " + articleId);
+                const response = await axios.get(`/api/articles/${articleId}`);
+                const newArticleInfo = response.data;
+                setArticleInfo(newArticleInfo);
+            }
+
+            loadArticleInfo();
+            
+        }, []);
+    
+    
+    const article = articles.find(article => article.name === articleId);
+   
+    const addUpvote = async () => {
+        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const updatedArticle = response.data;
+        setArticleInfo(updatedArticle);
+
+    };
+
+    if(!article)
+    {
+        return <NotFoundPage />
+    }
+    return (
+            <>
+            <h1>{article.title}</h1>
+            <div class="upvotes-section">
+                {user ?
+                <button onClick={addUpvote}>Upvote</button>
+                : <button>Login to upvote</button>
+                }
+                <p>This article has {articleInfo.upvotes} upvotes(s)</p>
+            </div>
+            {article.content.map((paragraph,i) => (
+                <p key={i}>{paragraph}</p>
+            ))}
+            { user ?
+            <AddCommentForm
+                articleName={articleId}
+                onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
+                : <button>Login to comment</button>}
+            <CommentsList comments={articleInfo.comments} />
+            </>
+    );
+
+}
+
+export default ArticlePage;
